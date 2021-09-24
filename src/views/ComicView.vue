641e70b879d222bf75103a380bf50676e74878e5
Bill Weisberger <bill.weisberger@gmail.com>
2021-09-24 15:07:00 -0600

    added grenade, working on hero refactor

--- src/views/ComicView.vue
+++ src/views/ComicView.vue
@@ -3,17 +3,16 @@
   .header
   Panel.panel
     template(#sentence='')
-      Sentence.text(@clicked-period="toggleActiveHero")
-    template(#hero-active='' v-if="activeHero")
-      img.hero.active(src="../assets/second-period-laser.gif")
-    template(#hero v-else)
-      img.hero.static(src="../assets/second-period.png" class="hero static")
+      Sentence.text(@punctuation-event1="toggleActiveHero1" @punctuation-event2="toggleActiveHero2")
+    template(#hero)
+      img.hero(:src="heroAsset")
+      img.grenade(src="../assets/Single_Grenade.png")
   .footer
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-
+import { mapGetters } from 'vuex';
 import { Panel, Sentence } from '@/components';
 export default defineComponent({
   name: 'ComicView',
@@ -21,17 +20,31 @@ export default defineComponent({
     Panel,
     Sentence,
   },
+  props: {
+    comicId: {
+      type: Number,
+      required: true
+    }
+  },
   data() {
     return {
-      activeHero: false
+      heroAsset: ''
     }
+  },
+  mounted() {
+    this.heroAsset = this.comicAssets(this.comicId).staticHeroAsset
+    console.log('heroAsset -->', this.heroAsset);
+  },
+  computed: {
+    ...mapGetters(['comicAssets'])
   },
   methods: {
     toggleActiveHero() {
-      this.activeHero = true;
-      setTimeout(() => {
-       this.activeHero = false 
-      }, 700);
+      // this.activeHero = true;
+      // this.
+      // setTimeout(() => {
+      //  this.activeHero = false 
+      // }, 700);
     }
   }
 })
@@ -42,6 +55,36 @@ export default defineComponent({
   height: 20vh;
   background-color: hotpink;
 }
+.grenade {
+  animation: parabola 2s infinite linear;
+}
+@keyframes rotation {
+  0% {
+    transform: rotate(359deg);
+  }
+  100% {
+    transform: rotate(0deg);
+  }
+}
+
+@keyframes parabola {
+  0% { 
+    transform: translate(0px, 0px) rotate(360deg); 
+  } 
+  25% { 
+    transform: translate(-200px, -100px) rotate(200deg); 
+  }
+  50% {
+    transform: translate(-400px, -200px) rotate(100deg); 
+  }
+  75% {
+    transform: translate(-600px, -80px) rotate(50deg); 
+  } 
+  100% {
+    transform: translate(-800px, -20px) rotate(0deg); 
+  } 
+}
+
 .panel {
   display: flex;
   align-items: center;
