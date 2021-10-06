0449ca82f9e9cd4e73aec1ca5acde04dd0c8d0cc
Bill Weisberger <bill.weisberger@gmail.com>
2021-10-06 16:23:36 -0600

    Add path param and comicAssets object

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,6 +2,19 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    comicAssets: <ComicAssetsMap>{
+      1: {
+        staticHeroAsset: '../assets/second-period.png',
+        activeHeroAsset1: '../assets/second-period-laser.gif',
+        activeHeroAsset2: '../assets/Grenade_Final.png',
+        heroObjectAsset2: '../assets/Single_Grenade.png'
+      }
+    }
+  },
+  getters: {
+    comicAssets: (state) => (comicId) => {
+      return state.comicAssets[comicId]
+    }
   },
   mutations: {
   },
