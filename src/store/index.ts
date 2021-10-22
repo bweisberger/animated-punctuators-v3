cdebc85fed4744cc12c6161c9de3526ad96c5916
Bill Weisberger <bill.weisberger@gmail.com>
2021-10-22 14:43:18 -0600

    update store

--- src/store/index.ts
+++ src/store/index.ts
@@ -8,7 +8,7 @@ export default createStore({
         staticHero1: 'second-period.png',
         activeHero1_1: 'second-period-laser.png',
         activeHero1_2: 'second-period-grenade.png',
-        hero1_entry: 'second-period-time-portal-enter.png',
+        hero1Entry: 'second-period-time-portal-enter.png',
         weapon1: 'laser.png',
         weapon2: 'black-hole-grenade.png',
         weapon2Effect: 'black-hole-2.png',
